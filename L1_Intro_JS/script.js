dfbdb6188f599fc6d1e17c6ac62e8cdedeb507d3
pratiyush ray <pkray0021@gmail.com>
2025-07-01 20:06:57 +0530

    first commit

---
+++ L1_Intro_JS/script.js
@@ -0,0 +1,26 @@
+console.log("HELLO WORLD");
+
+// setTimeout(()=>{
+//     console.log("I am SetTimeout")
+// },3000);
+
+
+var a =200;
+console.log(a);
+
+var b =3000;
+console.log(b);
+
+abc()
+
+function abc(){
+    console.log("HELLO I AM ABC FUNCTION")
+}
+
+function def(){
+    console.log("I am DEF FUnction")
+}
+
+def();
+
+console.log("END OF PROGRAM");
